f38d5b003b6684f8334770f5bb4b009dbefd0c7f
oducharles <ocharleslwanga@gmail.com>
2018-12-29 12:04:52 +0300

    tried the name cal;culator in the tutorial

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
 	'use strict';
 
 	var appName = angular.module('myFirstAngularJSApp', []);
@@ -8,6 +8,32 @@
 		$scope.sayHello = function() {
 
 			return "Hello everyone at coursera";
+		}
+	});
+})();*/
+
+(function() {
+	'use strict';
+
+	angular.module('BookCounter', [])
+	.controller('LibraryBooks', function($scope) {
+		// $scope.title = "";
+		$scope.bookname = "";
+		$scope.total_number_of_letters = 0;
+
+		$scope.get_numeric_total = function() {
+			var accumulated_total = accumulateTotal($scope.bookname);
+			$scope.total_number_of_letters = accumulated_total;
+
+		};
+
+		function accumulateTotal(string) {
+			var total_book_letters = 0;
+			for (var i = 0; i < string.length; i++) {
+				total_book_letters += string.charCodeAt(i);
+			}
+
+			return total_book_letters;
 		}
 	});
 })();
