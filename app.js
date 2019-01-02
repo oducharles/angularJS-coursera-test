f7a897648b9e6088d3958feb79c7db2eaeaf8675
oducharles <ocharleslwanga@gmail.com>
2019-01-02 10:20:40 +0300

    tried out code minifications

--- app.js
+++ app.js
@@ -11,12 +11,14 @@
 		}
 	});
 })();*/
-
+/*
 (function() {
 	'use strict';
 
 	angular.module('DIApp', [])
-	.controller('DIController', function($scope, $filter) {
+	.controller('DIController', ['$scope', '$filter', DIControllerFunction]);
+
+	function DIControllerFunction($scope, $filter) {
 		$scope.country_name = "";
 
 		$scope.to_upper_case = function() {
@@ -24,5 +26,7 @@
 			$scope.country_name = Upcase($scope.country_name)
 
 		};
-	});
-})();
+	}
+})();*/
+
+!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(n,e){n.country_name="",n.to_upper_case=function(){var o=e("uppercase");n.country_name=o(n.country_name)}}])}();
