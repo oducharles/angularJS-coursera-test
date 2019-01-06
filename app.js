9b2d082ab40e62071a1c91dd029f74f82a36d116
oducharles <ocharleslwanga@gmail.com>
2019-01-06 20:19:41 +0300

    Finished assignmemt 1

--- app.js
+++ app.js
@@ -26,21 +26,46 @@
 			var intro = $filter('uppercase')(my_intro);
 			return intro;
 		};
-		$scope.country_name = "chuck";
+		$scope.country_name = "OCl@chuck";
 		$scope.courseCost = .68;
 		$scope.stateOfBeing = "relaxed";
 
 
+		$scope.getItemsForLunch = "";
+		$scope.lunchMessage = "";
+
+
 		$scope.newMessage = function() {
-			return "Am taking on a course on AngularJS with coursera";
+			return "Am taking a course on AngularJS with coursera";
 		};
 		$scope.getToWork = function() {
 			$scope.stateOfBeing = "serious";
 		};
-
 		$scope.to_upper_case = function() {
 			var Upcase = $filter('uppercase');
 			$scope.country_name = Upcase($scope.country_name)
+		};
+
+
+		$scope.checkItemsForLunch = function() {
+			var lunch = $scope.getItemsForLunch;
+			var itemArray = lunch.split(', ');
+			
+			if (itemArray.length == 1) {
+				$scope.colorClass = "red";
+				$scope.borderColor = "red_border";
+				$scope.lunchMessage = "Please enter data first";
+			}
+			else if (itemArray.length <= 3) {
+				$scope.colorClass = "green";
+				$scope.borderColor = "green_border";
+				$scope.lunchMessage = "Enjoy";
+			}
+			else{
+				$scope.colorClass = "green";
+				$scope.borderColor = "green_border";
+				$scope.lunchMessage = "Too Much";
+			}
 
 		};
 	}
