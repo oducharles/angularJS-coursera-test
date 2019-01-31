9c32e8561a48d395d269e1b8e33d58f46380e4cc
oducharles <ocharleslwanga@gmail.com>
2019-01-31 20:57:54 +0300

    Finished assigmnet 2 after module 2. I also categorised module assignments into subfolders

--- Module-2/app.js
+++ Module-2/app.js
@@ -1,35 +1,85 @@
 (function() {
 	'use strict';
 
-	var shoppingList1 = [
-		"Black Books","Bic Pens","Towel","Soap","Tooth paste","Sugar",
-	];
+/*	var shoppingList1 = [
+		"Black Books","Bic Pens","Towel","Graph Books","Tooth paste","Art Books","Sugar",
+	];*/
 
-	var shoppingList2 = [
+	var myShopingList = [
 		{
 			name: "Hard Brush",
-			fine: "7000"
+			quantity: "1"
 		},
 		{
-			name: "Vim/Apic",
-			fine: "10000"
+			name: "Bathing Soap",
+			quantity: "3"
 		},
 		{
-			name: "Mopping rug",
-			fine: "3000"
+			name: "Mopping rugs",
+			quantity: "2"
 		},
 		{
 			name: "Toilet tissues",
-			fine: "5000"
+			quantity: "10"
+		},
+		{
+			name: "Black Books",
+			quantity: "5"
+		},
+		{
+			name: "Bic Pens",
+			quantity: "5"
+		},
+		{
+			name: "Tooth Paste",
+			quantity: "2"
 		},
 	];
-	angular.module('shoppingListApp', [])
-	.controller('shoppingListController', shoppingListController);
 
-	shoppingListController.$inject = ['$scope'];
-	function shoppingListController($scope) {
-		$scope.shoppingList1 = shoppingList1;
-		$scope.shoppingList2 = shoppingList2;
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', toBuyController)
+	.controller('AlreadyBoughtController', alreadyBoughtController)
+	.service('ShoppingListCheckOffService', shoppingListCheckOffService);
+
+	toBuyController.$inject = ['ShoppingListCheckOffService'];
+	alreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+
+	function toBuyController(shoppingListCheckOffService) {
+		var toBuy = this;
+		toBuy.list = myShopingList;
+		toBuy.addToBought = function(itemIndex) {
+			shoppingListCheckOffService.addToBought(toBuy.list[itemIndex].name, toBuy.list[itemIndex].quantity);
+			shoppingListCheckOffService.removeFromToBuy(itemIndex);
+		};
+	}
+
+	function alreadyBoughtController(shoppingListCheckOffService) {
+		var boughtItems = this;
+		boughtItems.list = shoppingListCheckOffService.getItems();
+	}
+
+	function shoppingListCheckOffService() {
+		var service = this;
+
+		var items = myShopingList;
+		var alreadybought = [];
+
+		service.removeFromToBuy = function(itemIndex) {
+			items.splice(itemIndex, 1);
+		};
+
+		service.addToBought = function(name, quantity) {
+			var item = {
+				name: name,
+				quantity: quantity
+			};
+			alreadybought.push(item);
+		};
+
+		service.getItems = function() {
+			return alreadybought;
+		};
 	}
 
 })();
